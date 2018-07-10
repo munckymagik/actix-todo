821c81811cd1c577f899fc0bc20fa020e413338e
Dan Munckton <dan@cultivatehq.com>
2018-07-10 15:34:27 +0100

    A response

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+extern crate actix_web;
+use actix_web::{server, App, HttpRequest, HttpResponse};
+
+fn index(_req: HttpRequest) -> HttpResponse {
+    HttpResponse::Ok().body("Hello world!")
+}
+
 fn main() {
-    println!("Hello, world!");
+    server::new(|| App::new().resource("/", |r| r.f(index)))
+        .bind("localhost:8088")
+        .unwrap()
+        .run()
 }
