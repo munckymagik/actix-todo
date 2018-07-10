19ad1d75cd4725b0ec80166fa32b12b9b64b2395
Dan Munckton <dan@cultivatehq.com>
2018-07-10 15:49:11 +0100

    Logging

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,24 @@
 extern crate actix_web;
+extern crate env_logger;
+
 use actix_web::{server, App, HttpRequest, HttpResponse};
+use actix_web::middleware::Logger;
 
 fn index(_req: HttpRequest) -> HttpResponse {
     HttpResponse::Ok().body("Hello world!")
 }
 
 fn main() {
-    server::new(|| App::new().resource("/", |r| r.f(index)))
+    std::env::set_var("RUST_LOG", "actix_web=info");
+    env_logger::init();
+
+    let app = || {
+        App::new()
+            .middleware(Logger::default())
+            .resource("/", |r| r.f(index))
+    };
+
+    server::new(app)
         .bind("localhost:8088")
         .unwrap()
         .run()
