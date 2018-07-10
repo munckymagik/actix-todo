07e1d5b5dff7d52bbe96ac6c09dff6406ae03d8b
Dan Munckton <dan@cultivatehq.com>
2018-07-10 16:17:07 +0100

    Get a template working

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,34 @@
 extern crate actix_web;
 extern crate env_logger;
+#[macro_use] extern crate tera;
 
 use actix_web::{server, App, HttpRequest, HttpResponse};
 use actix_web::middleware::Logger;
+use tera::{Context, Tera};
 
-fn index(_req: HttpRequest) -> HttpResponse {
-    HttpResponse::Ok().body("Hello world!")
+struct AppState {
+    template: Tera
+}
+
+fn index(req: HttpRequest<AppState>) -> HttpResponse {
+    let context = Context::new();
+    let rendered = req
+        .state().template
+        .render("index.html.tera", &context)
+        .expect("wow template");
+
+    HttpResponse::Ok().body(rendered)
 }
 
 fn main() {
     std::env::set_var("RUST_LOG", "actix_web=info");
     env_logger::init();
 
+
     let app = || {
-        App::new()
+        let tera: Tera = compile_templates!("templates/**/*");
+
+        App::with_state(AppState { template: tera })
             .middleware(Logger::default())
             .resource("/", |r| r.f(index))
     };
