bc0cfe48aaf74d4e83223608a3bd7f86108fb685
Dan Munckton <dan@cultivatehq.com>
2018-07-21 10:22:12 +0100

    Break out handlers

---
+++ src/handlers.rs
@@ -0,0 +1,143 @@
+use actix_web::{
+    http, AsyncResponder, Form, FutureResponse, HttpRequest,
+    HttpResponse, Path, State,
+};
+use actix_web::middleware::session::RequestSession;
+use futures::{future, Future};
+use tera::Context;
+
+use AppState;
+use db;
+
+#[derive(Deserialize, Serialize)]
+struct Flash {
+    kind: String,
+    message: String,
+}
+
+impl Flash {
+    fn success(message: &str) -> Self {
+        Self {
+            kind: "success".to_owned(),
+            message: message.to_owned(),
+        }
+    }
+
+    fn error(message: &str) -> Self {
+        Self {
+            kind: "error".to_owned(),
+            message: message.to_owned(),
+        }
+    }
+}
+
+#[derive(Deserialize)]
+pub struct CreateForm {
+    description: String,
+}
+
+#[derive(Deserialize)]
+pub struct UpdateParams {
+    id: i32,
+}
+
+#[derive(Deserialize)]
+pub struct UpdateForm {
+    _method: String,
+}
+
+macro_rules! flash {
+    ($req:expr, $flash:expr) => {
+        $req.session()
+            .set("flash", $flash)
+            .expect("failed to set cookie")
+    };
+}
+
+macro_rules! send_and_then {
+    ($db:expr, $message:expr, $block:expr) => {
+        $db.send($message).from_err().and_then($block).responder()
+    };
+}
+
+macro_rules! send_then_redirect {
+    ($db:expr, $message:expr) => {
+        send_and_then!($db, $message, |res| match res {
+            Ok(_) => Ok(redirect_to("/")),
+            Err(_) => Ok(internal_server_error()),
+        })
+    };
+}
+
+fn redirect_to(location: &str) -> HttpResponse {
+    HttpResponse::Found()
+        .header(http::header::LOCATION, location)
+        .finish()
+}
+
+fn bad_request() -> HttpResponse {
+    HttpResponse::BadRequest().body("400 Bad Request")
+}
+
+pub fn not_found() -> HttpResponse {
+    HttpResponse::NotFound().body("404 Not Found")
+}
+
+fn internal_server_error() -> HttpResponse {
+    HttpResponse::InternalServerError().body("500 Internal Server Error")
+}
+
+pub fn handle_index(req: HttpRequest<AppState>) -> FutureResponse<HttpResponse> {
+    send_and_then!(req.state().db, db::AllTasks, move |res| match res {
+        Ok(tasks) => {
+            let mut context = Context::new();
+            context.add("tasks", &tasks);
+
+            if let Some(flash) = req.session().get::<Flash>("flash")? {
+                context.add("msg", &(flash.kind, flash.message));
+                req.session().remove("flash");
+            }
+
+            let rendered = req.state()
+                .template
+                .render("index.html.tera", &context)
+                .expect("wow template");
+
+            Ok(HttpResponse::Ok().body(rendered))
+        }
+        Err(_) => Ok(internal_server_error()),
+    })
+}
+
+pub fn handle_create(
+    (req, params): (HttpRequest<AppState>, Form<CreateForm>),
+) -> FutureResponse<HttpResponse> {
+    if params.description.is_empty() {
+        flash!(req, Flash::error("Description cannot be empty"));
+        future::ok(redirect_to("/")).responder()
+    } else {
+        send_and_then!(
+            req.state().db,
+            db::CreateTask {
+                description: params.description.clone()
+            },
+            move |res| match res {
+                Ok(_) => {
+                    flash!(req, Flash::success("Task successfully added"));
+                    Ok(redirect_to("/"))
+                }
+                Err(_) => Ok(internal_server_error()),
+            }
+        )
+    }
+}
+
+pub fn handle_update_or_delete(
+    (state, params, form): (State<AppState>, Path<UpdateParams>, Form<UpdateForm>),
+) -> FutureResponse<HttpResponse> {
+    match form._method.as_ref() {
+        "put" => send_then_redirect!(state.db, db::ToggleTask { id: params.id }),
+        "delete" => send_then_redirect!(state.db, db::DeleteTask { id: params.id }),
+        _ => future::ok(bad_request()).responder(),
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -13,155 +13,21 @@ extern crate serde_derive;
 extern crate tera;
 
 use actix::prelude::{Addr, Syn, SyncArbiter};
-use actix_web::middleware::session::{CookieSessionBackend, RequestSession, SessionStorage};
+use actix_web::middleware::session::{CookieSessionBackend, SessionStorage};
 use actix_web::middleware::Logger;
 use actix_web::{
-    dev::ResourceHandler, fs, http, server, App, AsyncResponder, Form, FutureResponse, HttpRequest,
-    HttpResponse, Path, State,
+    dev::ResourceHandler, fs, http, server, App
 };
-use futures::{future, Future};
-use tera::{Context, Tera};
+use tera::Tera;
 
 mod db;
+mod handlers;
 mod schema;
 mod task;
 
-struct AppState {
+pub struct AppState {
     template: Tera,
     db: Addr<Syn, db::Conn>,
-}
-
-#[derive(Deserialize, Serialize)]
-struct Flash {
-    kind: String,
-    message: String,
-}
-
-impl Flash {
-    fn success(message: &str) -> Self {
-        Self {
-            kind: "success".to_owned(),
-            message: message.to_owned(),
-        }
-    }
-
-    fn error(message: &str) -> Self {
-        Self {
-            kind: "error".to_owned(),
-            message: message.to_owned(),
-        }
-    }
-}
-
-#[derive(Deserialize)]
-struct CreateForm {
-    description: String,
-}
-
-#[derive(Deserialize)]
-struct UpdateParams {
-    id: i32,
-}
-
-#[derive(Deserialize)]
-struct UpdateForm {
-    _method: String,
-}
-
-macro_rules! flash {
-    ($req:expr, $flash:expr) => {
-        $req.session()
-            .set("flash", $flash)
-            .expect("failed to set cookie")
-    };
-}
-
-macro_rules! send_and_then {
-    ($db:expr, $message:expr, $block:expr) => {
-        $db.send($message).from_err().and_then($block).responder()
-    };
-}
-
-macro_rules! send_then_redirect {
-    ($db:expr, $message:expr) => {
-        send_and_then!($db, $message, |res| match res {
-            Ok(_) => Ok(redirect_to("/")),
-            Err(_) => Ok(internal_server_error()),
-        })
-    };
-}
-
-fn redirect_to(location: &str) -> HttpResponse {
-    HttpResponse::Found()
-        .header(http::header::LOCATION, location)
-        .finish()
-}
-
-fn bad_request() -> HttpResponse {
-    HttpResponse::BadRequest().body("400 Bad Request")
-}
-
-fn not_found() -> HttpResponse {
-    HttpResponse::NotFound().body("404 Not Found")
-}
-
-fn internal_server_error() -> HttpResponse {
-    HttpResponse::InternalServerError().body("500 Internal Server Error")
-}
-
-fn handle_index(req: HttpRequest<AppState>) -> FutureResponse<HttpResponse> {
-    send_and_then!(req.state().db, db::AllTasks, move |res| match res {
-        Ok(tasks) => {
-            let mut context = Context::new();
-            context.add("tasks", &tasks);
-
-            if let Some(flash) = req.session().get::<Flash>("flash")? {
-                context.add("msg", &(flash.kind, flash.message));
-                req.session().remove("flash");
-            }
-
-            let rendered = req.state()
-                .template
-                .render("index.html.tera", &context)
-                .expect("wow template");
-
-            Ok(HttpResponse::Ok().body(rendered))
-        }
-        Err(_) => Ok(internal_server_error()),
-    })
-}
-
-fn handle_create(
-    (req, params): (HttpRequest<AppState>, Form<CreateForm>),
-) -> FutureResponse<HttpResponse> {
-    if params.description.is_empty() {
-        flash!(req, Flash::error("Description cannot be empty"));
-        future::ok(redirect_to("/")).responder()
-    } else {
-        send_and_then!(
-            req.state().db,
-            db::CreateTask {
-                description: params.description.clone()
-            },
-            move |res| match res {
-                Ok(_) => {
-                    flash!(req, Flash::success("Task successfully added"));
-                    Ok(redirect_to("/"))
-                }
-                Err(_) => Ok(internal_server_error()),
-            }
-        )
-    }
-}
-
-fn handle_update_or_delete(
-    (state, params, form): (State<AppState>, Path<UpdateParams>, Form<UpdateForm>),
-) -> FutureResponse<HttpResponse> {
-    match form._method.as_ref() {
-        "put" => send_then_redirect!(state.db, db::ToggleTask { id: params.id }),
-        "delete" => send_then_redirect!(state.db, db::DeleteTask { id: params.id }),
-        _ => future::ok(bad_request()).responder(),
-    }
 }
 
 fn main() {
@@ -186,13 +52,13 @@ fn main() {
             .middleware(SessionStorage::new(
                 CookieSessionBackend::signed(&[0; 32]).secure(false),
             ))
-            .route("/", http::Method::GET, handle_index)
+            .route("/", http::Method::GET, handlers::handle_index)
             .resource("/todo/{id}", |r: &mut ResourceHandler<_>| {
-                r.post().with(handle_update_or_delete)
+                r.post().with(handlers::handle_update_or_delete)
             })
-            .route("/todo", http::Method::POST, handle_create)
+            .route("/todo", http::Method::POST, handlers::handle_create)
             .handler("/static", fs::StaticFiles::new("static/"))
-            .default_resource(|r: &mut ResourceHandler<_>| r.f(|_| not_found()))
+            .default_resource(|r: &mut ResourceHandler<_>| r.f(|_| handlers::not_found()))
     };
 
     debug!("Starting server");
